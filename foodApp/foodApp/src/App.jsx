ceb4ed6de56997f4341eef1b85742f3830065c48
Avinash <avinashkumarofficial95@gmail.com>
2025-04-10 18:22:22 +0530

    installed tailwind in my application

--- foodApp/foodApp/src/App.jsx
+++ foodApp/foodApp/src/App.jsx
@@ -1,9 +1,13 @@
 import './App.css'
-
+import Header from './component/header'
+import Footer from './component/footer'
+import Body from './component/body'
 function App() {
   return (
     <>
-      
+      <Header></Header>
+      <Body></Body>
+      <Footer></Footer>
     </>
   )
 }
---
+++ foodApp/foodApp/src/component/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <>
+        <div>
+            <img src="https://s3.amazonaws.com/cdn.designcrowd.com/blog/39-Food-Delivery-Logos-That-Will-Leave-You-Hungry-For-More/food-app-by-town-brandcrowd.png" 
+            alt="" />
+        </div>
+    </>
+    
+  )
+}
+
+export default Header
