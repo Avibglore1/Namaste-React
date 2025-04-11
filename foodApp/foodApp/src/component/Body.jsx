ec66c2ae40f8b23387b928e7c16b5f5f8ece0fb5
Avinash <avinashkumarofficial95@gmail.com>
2025-04-11 16:28:28 +0530

    understood diffence b/e normal variable and react state variable

--- foodApp/foodApp/src/component/Body.jsx
+++ foodApp/foodApp/src/component/Body.jsx
@@ -1,13 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import RestaurantCard from './RestaurantCard'
-import { restrautList } from '../utils/mockData'
+import {restrautList as restaurantList} from "./../utils/mockData"
+
+
 function body() {
+  const [restrautList,setrestraList] = useState(restaurantList);
+
+  function filteredList(){
+    setrestraList(restrautList.filter(restrau => restrau.data.avgRating>4))
+  }
+
   return (
     <div className='space-y-3'>
       <input type="text" placeholder="Search for food..." 
         className='border border-gray-300 rounded-l px-4 py-2 ml-5 focus:outline-none focus:ring-2 focus:ring-orange-500'
       />
-      <button className='bg-orange-500 text-white px-4 py-2 rounded-r hover:bg-orange-600 transition-colors'>
+      <button className='bg-orange-500 text-white px-4 py-2 rounded-r hover:bg-orange-600 transition-colors'
+      onClick={filteredList}>
         Search
       </button> 
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
@@ -23,6 +32,3 @@ function body() {
 
 export default body
 
-// {restrautList.map((item) => (
-//   <RestaurantCard key={item.data.id} restaurant={item.data} />
-// ))}
