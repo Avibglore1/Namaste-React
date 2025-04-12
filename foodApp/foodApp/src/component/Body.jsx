9cf8a97680e364f34802d4799768992691665346
Avinash <avinashkumarofficial95@gmail.com>
2025-04-12 18:00:38 +0530

    implemented search alorithm for filtering out movie based on user search text

--- foodApp/foodApp/src/component/Body.jsx
+++ foodApp/foodApp/src/component/Body.jsx
@@ -5,6 +5,7 @@ import Shimmer from './Shimmer';
 
 function Body() {
   const [movieList,setMovieList] = useState([]);
+  const [searchText,setSearchText] = useState("");
 
   useEffect(()=>{
     fetchData();
@@ -15,23 +16,30 @@ function Body() {
     const json = await data.json();
     setMovieList(json.results)
     console.log(json);
-    
   }
   
   function filteredList(){
     setMovieList(movieList.filter(movie => movie.vote_average>7.5))
   }
 
-  
   return movieList.length === 0 ? (<Shimmer />) :(
     <div className='space-y-3'>
       <input type="text" placeholder="Search for movie..." 
         className='border border-gray-300 rounded-l px-4 py-2 ml-5 focus:outline-none focus:ring-2 focus:ring-orange-500'
+        value={searchText} onChange={(e)=>{
+          setSearchText(e.target.value);
+        }}
       />
       <button className='bg-orange-500 text-white px-4 py-2 rounded-r hover:bg-orange-600 transition-colors'
-      onClick={filteredList}>
+      onClick={()=>{
+        const searchMovieList = movieList.filter((movie)=>movie.original_title.includes(searchText));
+        setMovieList(searchMovieList);
+      }}
+      >
         Search
       </button> 
+      <button className='bg-slate-500 text-white px-4 py-2 rounded-r hover:bg-slate-600 transition-colors ml-80'
+      onClick={filteredList}>fiteredMovie</button>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
       {movieList.map((item)=>{ 
         return(
