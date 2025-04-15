16a0170139c25da0a320c6d281bc0beaa07239d8
Avinash <avinashkumarofficial95@gmail.com>
2025-04-15 12:42:58 +0530

    delclaring state variables in class based component

--- foodApp/foodApp/src/component/User.jsx
+++ foodApp/foodApp/src/component/User.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function User({name,location}) {
+    const [count,setCount] = useState(0);
+    const [sum,setSum] = useState(100);
   return (
-    <div className='border-4 border-cyan-200 text-center'>
+    <div className='border-4 border-cyan-200 text-center m-2'>
         <h2>{name}</h2>
+        <h3>Count: {count}</h3>
+        <h3>Sum: {sum}</h3>
         <h3>Location: {location}</h3>
         <h3>Contact: avinash@gmail.com</h3>
     </div>
--- foodApp/foodApp/src/component/UserClass.jsx
+++ foodApp/foodApp/src/component/UserClass.jsx
@@ -3,12 +3,19 @@ import React from "react"
 class Userclass extends React.Component{
     constructor(props){
         super(props);
+        this.state={
+            count: 0,
+            sum : 100
+        };
     }
     render(){
         const {name,location} = this.props;
+        const {count,sum} = this.state;
         return(
             <div className='border-4 border-cyan-200 text-center'>
             <h2>{name}</h2>
+            <h3>Count: {count}</h3>
+            <h3>Sum: {sum}</h3>
             <h3>Location: {location}</h3>
             <h3>Contact: avinash@gmail.com</h3>
             </div> 
