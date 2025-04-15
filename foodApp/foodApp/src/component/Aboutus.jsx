e79fc20a3a5467f11d5a7bcc5ee7edc4a0b06325
Avinash <avinashkumarofficial95@gmail.com>
2025-04-15 15:59:44 +0530

    made an api call from class based component and used it result to show in ui

--- foodApp/foodApp/src/component/Aboutus.jsx
+++ foodApp/foodApp/src/component/Aboutus.jsx
@@ -4,22 +4,34 @@ import UserClass from "./UserClass"
 
 class Aboutus extends React.Component{
   constructor(props){
-    super(props)
-    console.log('constructor called');
+    super(props);
+    this.state={
+      userInfo:{
+        name:"Dummy",
+        avatar_url:''
+      }
+    }
+    // console.log('constructor called');
     
   }
-  componentDidMount(){
-    console.log("Component did mount");
+  async componentDidMount(){
+    // console.log("Component did mount");
+    const data = await fetch('https://api.github.com/users/Avibglore1');
+    const json = await data.json();
+    this.setState({
+      userInfo:json
+    })
+    console.log('data',json);
     
   }
   render(){
-    console.log('render is called');
-    
+    // console.log('render is called');
+    const {name,avatar_url} = this.state.userInfo
     return (
           <>
             <div>Aboutus
               {/* <User name={'Avinash function'} location={'Jamshedpur'}></User> */}
-              <UserClass name={'Avinash class'} location={'Jamshedpur'}/>
+              <UserClass name={name} avatar_url={avatar_url}/>
             </div>
           </>
           
--- foodApp/foodApp/src/component/UserClass.jsx
+++ foodApp/foodApp/src/component/UserClass.jsx
@@ -8,24 +8,24 @@ class Userclass extends React.Component{
             sum : 100
         };
 
-        console.log('Child constructor called');
+        // console.log('Child constructor called');
     }
     componentDidMount(){
-        console.log('Child component did mount');
+        // console.log('Child component did mount');
         
     }
     render(){
-        const {name,location} = this.props;
+        const {name,avatar_url} = this.props;
         const {count,sum} = this.state;
 
-        console.log('Child render called');
+        // console.log('Child render called');
         
         return(
             <div className='border-4 border-cyan-200 text-center'>
             <h2>{name}</h2>
             <h3>Count: {count}</h3>
             <h3>Sum: {sum}</h3>
-            <h3>Location: {location}</h3>
+            <img src={avatar_url}/>
             <h3>Contact: avinash@gmail.com</h3>
             <button onClick={()=>{
                 this.setState({
